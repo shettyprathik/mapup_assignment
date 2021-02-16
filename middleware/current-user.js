f4cc090596a29e63cedc68358c47d007b02f8514
shettyPrathik <prathik.gunapal@gmail.com>
2021-02-16 22:05:36 +0530

    ternary check for cookies

--- middleware/current-user.js
+++ middleware/current-user.js
@@ -2,7 +2,7 @@ const { verifyJwt } = require('../utils/jwt');
 const users = require('../models/users');
 
 const currentUser = async (req, res, next) => {
-  const token = req.cookies?.jwt;
+  const token = req.cookies ? req.cookies.jwt : undefined;
   try {
     const decode = verifyJwt(token);
     req.currentUser = users.filter((user) => user.id == decode.id);
